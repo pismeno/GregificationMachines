f0ed9372ea973316dadf78ca7ef6f824ec07ac40
pismeno <kyky151cze@gmail.com>
2025-03-07 22:30:41 +0100

    - added a slot for programmed circuit in Saw

--- src/main/java/pismeno/gfmachines/common/recipes/RecipeMaps.java
+++ src/main/java/pismeno/gfmachines/common/recipes/RecipeMaps.java
@@ -1,5 +1,6 @@
 package pismeno.gfmachines.common.recipes;
 
+import gregtech.api.gui.GuiTextures;
 import gregtech.api.gui.widgets.ProgressWidget;
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
@@ -13,9 +14,10 @@ public class RecipeMaps {
 
 
     public static void preInit() {
-           SAW_RECIPES = new RecipeMap<>("saw", 1, 3, 0, 0,
+           SAW_RECIPES = new RecipeMap<>("saw", 2, 3, 0, 0,
                 new SimpleRecipeBuilder().duration(200).EUt(2), false)
-                .setSlotOverlay(false, false, GuiTexturesHere.SAW_OVERLAY)
+                   .setSlotOverlay(false, false, false, GuiTexturesHere.SAW_OVERLAY)
+                   .setSlotOverlay(false, false, true, GuiTextures.INT_CIRCUIT_OVERLAY)
                 .setProgressBar(GuiTexturesHere.PROGRESS_BAR_SAW, ProgressWidget.MoveType.HORIZONTAL)
                 .setSound(GTSoundEvents.COMPRESSOR);
     }
