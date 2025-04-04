f6563e18856592f8d647b8da5b518e00a47ee26d
pismeno <kyky151cze@gmail.com>
2025-04-04 16:02:24 +0200

    - trying to fix Laser Fabricator RecipeMap

--- src/main/java/pismeno/gfmachines/GfMachines.java
+++ src/main/java/pismeno/gfmachines/GfMachines.java
@@ -8,36 +8,34 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import org.spongepowered.asm.launch.MixinBootstrap;
-import org.spongepowered.asm.mixin.Mixins;
 import pismeno.gfmachines.client.Textures;
 import pismeno.gfmachines.common.metatileentities.MetaTileEntities;
+import pismeno.gfmachines.common.recipes.MachineRecipes;
 import pismeno.gfmachines.common.recipes.RecipeMaps;
 
-@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.12.2]")
+@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.12.2]", dependencies =  "required-after:gregtech")
 public class GfMachines {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MODNAME);
 
     @EventHandler
-    // preInit Run before anything else. Read your config, create blocks, items, etc. (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
-        // register to the event bus so that we can listen to events
         MinecraftForge.EVENT_BUS.register(this);
         LOGGER.info("Recognized " + Tags.MODNAME + " + at version " + Tags.VERSION);
+
         Textures.preInit();
-        RecipeMaps.preInit();
-        LOGGER.info("Textures pre initialized");
     }
 
     @EventHandler
-    // load "Do your mod setup. Build whatever data structures you care about." (Remove if not needed)
     public void init(FMLInitializationEvent event) {
         MetaTileEntities.init();
-        LOGGER.info("Meta Tile Entities initialized");
+    }
+
+    @EventHandler
+    public void registerRecipes(RegistryEvent.Register<IRecipe> event) {
+        MachineRecipes.init();
     }
 }
---
+++ src/main/java/pismeno/gfmachines/common/recipes/MachineRecipes.java
@@ -0,0 +1,17 @@
+package pismeno.gfmachines.common.recipes;
+
+import gregtech.api.unification.material.Materials;
+import gregtech.api.unification.ore.OrePrefix;
+
+import static gregtech.api.GTValues.*;
+import static pismeno.gfmachines.common.recipes.RecipeMaps.*;
+
+public class MachineRecipes {
+    public static void init() {
+        LASER_FABRICATOR_RECIPES.recipeBuilder()
+                .input(OrePrefix.ingot, Materials.Silver)
+                .input(OrePrefix.plate, Materials.Tungsten)
+                .output(OrePrefix.plate, Materials.Steel)
+                .duration(240).EUt(VA[HV]).buildAndRegister();
+    }
+}
